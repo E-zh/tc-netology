73a818dbfd9f5562f1509953bbfd3e3ef27b1a7a
Egor Zhelobanov <e.zhelobanov@gmail.com>
2022-12-21 20:27:30 +1100

    Added meta

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
 
 provider yandex {
   token     = var.yc_token
-  #service_account_key_file = file("key.json")
+  #ervice_account_key_file = file("key.json")
   cloud_id  = var.yc_cloud_id
   folder_id = var.yc_folder_id
   zone      = "ru-central1-a"
@@ -31,50 +31,8 @@ resource "yandex_vpc_subnet" "subnet" {
   v4_cidr_blocks = ["192.168.1.0/24"]
 }
 
-locals {
-  instance = {
-    default = 1
-    stage = 1
-    prod  = 2
-  }
-}
-
-resource "yandex_compute_instance" "netology-count" {
-  name = "netology-${count.index}-${terraform.workspace}"
-
-  resources {
-    cores  = "2"
-    memory = "4"
-  }
-
-  boot_disk {
-    initialize_params {
-      image_id = yandex_compute_image.ubu-img.id
-    }
-  }
-
-  network_interface {
-    subnet_id = yandex_vpc_subnet.subnet.id
-    nat       = true
-  }
-
-  count = local.instance[terraform.workspace]
-
-  metadata = {
-    user-data = file("meta.txt")
-  }
-}
-
-locals {
-  id = toset([
-    "1",
-    "2",
-  ])
-}
-
-resource "yandex_compute_instance" "netology-for" {
-  for_each = local.id
-  name     = "netology-${each.key}-${terraform.workspace}"
+resource "yandex_compute_instance" "netology" {
+  name = "netology"
 
   resources {
     cores  = "2"
