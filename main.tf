b0e55b7ae7e3ec281eb1c0f662ae0b19e66ee606
Egor Zhelobanov <e.zhelobanov@gmail.com>
2022-12-21 22:56:54 +1100

    Fixed

--- main.tf
+++ main.tf
@@ -1,21 +1,8 @@
 provider "yandex" {
   token     = var.yc_token
-  #ervice_account_key_file = file("key.json")
   cloud_id  = var.yc_cloud_id
   folder_id = var.yc_folder_id
   zone      = "ru-central1-a"
-}
-
-terraform {
-  backend "netology" {
-    endpoint = "storage.yandexcloud.net"
-    bucket   = "terraform-dev-state"
-    region   = "ru-central1-a"
-    key      = "dev/terraform.tfstate"
-
-    skip_region_validation      = true
-    skip_credentials_validation = true
-  }
 }
 
 module "compute" {
@@ -26,15 +13,16 @@ module "compute" {
   subnet       = "subnetology"
   folder_id    = var.yc_folder_id
 
-  name     = "development"
-  hostname = "dev"
-  is_nat   = true
+  name              = "netology"
+  hostname          = "netology"
+  is_nat            = false
+  secondary_disk_id = ""
 
   cores  = 2
   memory = 4
   size   = "10"
 
-  preemptible = true
+  preemptible = false
 
   sg_id = ["default"]
 
