c10741400350bda391fb5fd82824e4896bb21140
Egor Zhelobanov <e.zhelobanov@gmail.com>
2022-12-21 22:42:19 +1100

    Fixed

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
+provider yandex {
+  token     = var.yc_token
+  cloud_id  = var.yc_cloud_id
+  folder_id = var.yc_folder_id
+  zone      = "ru-central1-a"
+}
+
 module "yc-instance" {
   source             = "github.com/olezhuravlev/terraform-yandex-compute-instance.git"
   yandex_cloud_id    = var.yc_cloud_id
