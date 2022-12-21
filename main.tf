34c772e1f240de90545ac2e559b0153d77416304
Egor Zhelobanov <e.zhelobanov@gmail.com>
2022-12-21 22:40:28 +1100

    Fixed

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "yc-instance" {
-  source             = "./modules/yc-compute-instance"
+  source             = "github.com/olezhuravlev/terraform-yandex-compute-instance.git"
   yandex_cloud_id    = var.yc_cloud_id
   folder_id          = var.yc_folder_id
   path_to_public_key = "~/.ssh/id_rsa.pub"
